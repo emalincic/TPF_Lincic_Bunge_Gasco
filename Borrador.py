d7a7c56a47abd0480116d5da0453b70ad8f8edce
SantosBunge <bunges@udesa.edu.ar>
2025-06-03 17:05:20 -0300

    Combinacion grilla facu, soles Santos

--- Borrador.py
+++ Borrador.py
@@ -3,9 +3,78 @@ import random
 import BC
 import SUNS
 from pygame.locals import RLEACCEL
+# Clase Plantas
+class Plants(pygame.sprite.Sprite):
+    def __init__(self, image_file, pos, dims = (800, 600), cost = 50, life = 100):
+        super(Plants, self).__init__()
+        non_dimmed = pygame.image.load(image_file).convert_alpha()
+        self.image = pygame.transform.scale(non_dimmed, (80, 80))
+        self.pos = pos
+        self.rect = self.image.get_rect(center=pos)
+        self.cost = cost
+        self.state = pygame.time.get_ticks()
+        self.ready = None
+        self.life = life
+    def take_damage(self, damage = 0):
+        if self.life <= 0: self.kill()
+        else: self.life -= damage
+    def remove(self): 
+        self.kill()
+        return int(self.cost/2)
 
-# Inicializamos pygame
+class Sunflower(Plants):
+    def __init__(self, image_file, pos, dims=(800, 600), cost=50, life = 100):
+        super().__init__(image_file, pos, dims, cost, life)
+    def ability(self):
+        if self.ready == None:
+            self.ready = pygame.time.get_ticks()
+        elif pygame.time.get_ticks() - self.ready >= 10000:
+            fpy = self.rect.centery
+            sun = SUNS.SF_sun('Images/sol.png', self.rect.center, fpy)
+            soles.add(sun)
+            self.ready = None
+
+class Nut(Plants):
+    def __init__(self, image_file, pos, dims=(800, 600), cost=50, life=200):
+        super().__init__(image_file, pos, dims, cost, life)
+    def ability(self): return None
+
+class PeaShotter(Plants):
+    def __init__(self, image_file, pos, pea_file, dims=(800, 600), cost=50, life=100):
+        super().__init__(image_file, pos, dims, cost, life)
+        self.pea_file = pea_file
+    def ability(self):
+        if self.ready == None:
+            self.ready = pygame.time.get_ticks()
+        elif pygame.time.get_ticks() - self.ready >= 5000:
+            new_pea = Pea(self.pea_file, (self.pos))
+            peas.add(new_pea)
+            self.ready = None
+
+class Pea(pygame.sprite.Sprite):
+    def __init__(self, image_file, pos, dims=(800, 600)):
+        super(Pea, self).__init__()
+        non_dimmed = pygame.image.load(image_file).convert_alpha()
+        self.image = pygame.transform.scale(non_dimmed, (50, 50))
+        self.pos = pos
+        self.rect = self.image.get_rect(center=pos)
+    def shoot(self):
+        self.rect.move_ip(1, 0)
+
+# Intizialize pygame
 pygame.init()
+soles = pygame.sprite.Group()
+girasoles = pygame.sprite.Group()
+pea_shooters = pygame.sprite.Group()
+nuts = pygame.sprite.Group()
+peas = pygame.sprite.Group()
+
+def get_all_palnts():
+    return girasoles.sprites() + pea_shooters.sprites() + nuts.sprites()
+
+
+sunflower_cooldown = 10000
+last_sunflower_placed = -sunflower_cooldown # Tratar de incorporar cooldowns en las clases??
 
 # Creamos ventana
 dims = (1200, 600)
@@ -21,8 +90,6 @@ pygame.time.set_timer(SUN_EVENT, 5000)
 
 # Grupo de soles
 soles = pygame.sprite.Group()
-
-
 
 
 # ========================
@@ -81,6 +148,22 @@ while run:
             for sol in soles:
                 if sol.rect.collidepoint(event.pos):
                     sun_counter += sol.grab()
+        # elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 3:
+        #     if time - last_sunflower_placed >= sunflower_cooldown:
+        #         new_sunflower = PeaShotter('Images/Peashooter.png', pygame.mouse.get_pos(), 'Images/Pea.png')
+        #         pea_shooters.add(new_sunflower)
+        #         last_sunflower_placed = time
+        #     else: print("Not ready") # ACA HABRIA QUE IMPLEMENTAR LO QUE PASA EN EL COOLDOWN DE LAS PLANTAS
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_p:
+                new_peashooter = PeaShotter('Images/Peashooter.png', pygame.mouse.get_pos(), 'Images/Pea.png')
+                pea_shooters.add(new_peashooter)
+            elif event.key == pygame.K_g:
+                    new_sunflower = Sunflower('Images/Sunflower.png', pygame.mouse.get_pos())
+                    girasoles.add(new_sunflower)
+            elif event.key == pygame.K_n:
+                new_nut = Nut('Images/Nut.png', pygame.mouse.get_pos())
+                nuts.add(new_nut)
 
     soles.update()
     ##screen.fill((0, 0, 0))
@@ -98,6 +181,12 @@ while run:
         for j in range(rows):
             rect = pygame.Rect(i * cell_width, j * cell_height, cell_width, cell_height)
             pygame.draw.rect(screen, (0, 100, 0), rect, 2)  # Borde verde oscuro
+    for plant in get_all_palnts():
+        plant.ability()
+        screen.blit(plant.image, plant.rect)
+    for pea in peas:
+        pea.shoot()
+        screen.blit(pea.image, pea.rect)
     for sol in soles:
         sol.action()
         screen.blit(sol.image, sol.rect)
--- SUNS.py
+++ SUNS.py
@@ -1,55 +1,45 @@
 import random
 import pygame
 
+
 def cell_center(cols, rows, key, pos = None, dims=(1200, 600)):
+    cell_width = dims[0] // cols
+    cell_height = dims[1] // rows
     if key == 'plant':
-        cell_width = dims[0] // cols
-        cell_height = dims[1] // rows
         col = pos[0] // cell_width
         row = pos[1] // cell_height
-
         center_x = col * cell_width + cell_width // 2
         center_y = row * cell_height + cell_height // 2
         return (center_x, center_y)
     elif key == 'sun':
-        cell_width = dims[0] // cols
-        cell_height = dims[1] // rows
         # Elegimos columnas 1 a 9 y filas 1 a 5 (ignoramos la fila y columna 0)
-        col = random.randint(1, cols - 1)   # 1 a 9
-        row = random.randint(1, rows - 1)   # 1 a 5
-
-
-    
-
+        first_col, last_call = 1, (cols - 2)
+        first_row, last_row = 1, (rows - 1)
+        col = random.randint(first_col, last_call)
+        row = random.randint(first_row, last_row)
+        cell_x = col * cell_width
+        cell_y = row * cell_height
+        cx = random.randint(cell_x, cell_x + cell_width - 1)
+        cy = random.randint(cell_y, cell_y + cell_height - 1)
+        return (cx, cy)
+       
 # Clase soles
 class Suns(pygame.sprite.Sprite):
-    def __init__(self, image_file, dims=(1200, 600), value=50):
+    def __init__(self, image_file, start_pos = None, fpy = None, dims=(1200, 600), value=50, cols = 10, rows = 6):
         super(Suns, self).__init__()
         non_dimmed = pygame.image.load(image_file).convert_alpha()
         self.image = pygame.transform.scale(non_dimmed, (80, 80))
-
-        # Tamaño de la grilla (10x6)
-        cols = 10
-        rows = 6
-        cell_width = dims[0] // cols
-        cell_height = dims[1] // rows
-
-        # Elegimos columnas 1 a 9 y filas 1 a 5 (ignoramos la fila y columna 0)
-        col = random.randint(1, cols - 1)   # 1 a 9
-        row = random.randint(1, rows - 1)   # 1 a 5
-
-        # Coordenadas del centro de la celda elegida
-        center_x = col * cell_width + cell_width // 2
-        start_y = 0  # Empieza arriba
-        end_y = row * cell_height + cell_height // 2  # Final en el centro de la celda
-
-        self.rect = self.image.get_rect(center=(center_x, start_y))
-        self.final_pos = (center_x, end_y)
+        if fpy == None or start_pos == None:
+            final_pos = cell_center(cols, rows, 'sun', None)
+            self.rect = self.image.get_rect(center=(final_pos[0], 0))
+            self.final_pos = (final_pos[0], final_pos[1])
+        else:
+            self.rect = self.image.get_rect(center=(start_pos))
+            self.final_pos = (start_pos[0], start_pos[1])
 
         self.state = True
         self.value = value
         self.time = None
-
     def action(self):
         if self.rect.center == self.final_pos:
             if self.time is None:
@@ -63,11 +53,6 @@ class Suns(pygame.sprite.Sprite):
     def grab(self):
         self.kill()
         return self.value
-
-
-
-
-
 
 class SF_sun(Suns):
     def __init__(self, image_file, start_pos, fpy, value=50):
